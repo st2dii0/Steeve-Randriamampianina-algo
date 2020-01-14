d504ceee77fdc5cbeb36e2f316be45f9539679ef
Steeve R <st2diio@protonmail.ch>
2020-01-14 15:08:24 +0100

    exos

---
+++ exo/ex01.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def unique(str):
+    for letter in str:
+        counter = str.count(letter)
+        while counts > 1:
+            return print(letter, counter)
---
+++ exo/ex02.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def reverse(str):
+    return str[::-1]
+
+if __name__ == "__main__":
+    reverse('jetebaise')
