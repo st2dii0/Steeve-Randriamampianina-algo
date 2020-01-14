53f807bf62ca68a3eaed56497b566473c65af979
Steeve R <st2diio@protonmail.ch>
2020-01-14 15:38:31 +0100

    exos

--- exo/ex02.py
+++ exo/ex02.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 
 def reverse(str):
-    return str[::-1]
-
-if __name__ == "__main__":
-    reverse('jetebaise')
+    # return str[::-1] # easy answer
+    
+    # other one
+    rev = ''
+    i = len(str)
+    while i > 0:
+        rev += str[ i - 1]
+        i -= 1
+    return rev
