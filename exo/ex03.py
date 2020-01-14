c88d51aed78dedf5cf083f7975e2bf09ee1439d1
Steeve R <st2diio@protonmail.ch>
2020-01-14 15:17:55 +0100

    exos

--- exo/ex03.py
+++ exo/ex03.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 
-def 
+def anagrams(str1, str2):
+    length1 = len(str1)
+    length2 = len(str2)
+
+    if length1 != length2:
+        return 0
+    else:
+        str1 = sorted(str1)
+        str2 = sorted(str2)
+        for i in range(0, length1):
+            if str1[i] != str2[i]:
+                return 0
+        return 1
